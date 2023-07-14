604a2c7c1928f867654e88feda203e34ed6854df
pablonoya <pablonoya18@live.com>
2023-07-14 11:34:32 -0400

    reorganize theme.lua

--- theme/theme.lua
+++ theme/theme.lua
@@ -1,16 +1,13 @@
 local naughty = require("naughty")
 local gsurface = require("gears.surface")
 local gfs = require("gears.filesystem")
-
-local themes_path = gfs.get_themes_dir()
-local theme = dofile(themes_path .. "default/theme.lua")
-
 local xrdb = require("beautiful.xresources").get_current_theme()
 
 local helpers = require("helpers")
 
 local local_dir = gfs.get_configuration_dir()
 local HOME = os.getenv("HOME")
+local theme = {}
 
 -- Load ~/.Xresources colors
 theme.xbackground = xrdb.background
@@ -49,32 +46,6 @@ theme.mercury = theme.blue
 theme.saturn = theme.magenta
 theme.neptune = theme.cyan
 
--- Accent colors
-theme.accent = theme.blue
-theme.focus = "#424f5b"
-
--- Time of the day colors
-theme.late_night_color = theme.magenta
-theme.dawn_color = theme.pluto
-theme.morning_color = theme.yellow
-theme.midday_color = theme.moon
-theme.dusk_color = theme.chibimoon
-theme.night_color = theme.blue
-
--- PFP
-theme.pfp = gsurface.load_uncached(local_dir .. "theme/assets/kirby_snowboard.jpg")
-
--- Notification icons
-theme.notification_icon = gsurface.load_uncached(local_dir .. "icons/notification.svg")
-theme.notification_bell_icon = gsurface.load_uncached(local_dir .. "icons/notification_bell.svg")
-
--- Fonts
-theme.font_name = "Manrope "
-theme.font = theme.font_name .. "10"
-theme.icon_font_name = "Material Icons "
-theme.icon_font = theme.icon_font_name .. "16"
-theme.mono_font = "JetBrains Mono Slashed 10"
-
 -- Background Colors
 theme.bg_dark = theme.black
 theme.bg_normal = theme.xbackground
@@ -88,6 +59,28 @@ theme.bg_accent = theme.focus
 theme.fg_normal = theme.xforeground
 theme.fg_focus = theme.accent
 
+-- Accent colors
+theme.accent = theme.blue
+theme.focus = "#424f5b"
+
+-- Time of the day colors
+theme.late_night_color = theme.magenta
+theme.dawn_color = theme.pluto
+theme.morning_color = theme.yellow
+theme.midday_color = theme.moon
+theme.dusk_color = theme.chibimoon
+theme.night_color = theme.blue
+
+-- Gaps
+theme.useless_gap = dpi(4)
+
+-- Fonts
+theme.font_name = "Manrope "
+theme.font = theme.font_name .. "10"
+theme.icon_font_name = "Material Icons "
+theme.icon_font = theme.icon_font_name .. "16"
+theme.mono_font = "JetBrains Mono Slashed 10"
+
 -- Borders
 theme.border_width = dpi(2)
 theme.border_normal = theme.focus
@@ -97,19 +90,32 @@ theme.border_activate = theme.accent
 theme.border_marked = theme.uranus
 theme.widget_border_width = dpi(2)
 
--- Radius
+-- Border radius
 theme.border_radius = dpi(16)
 theme.client_border_radius = dpi(12)
-
 theme.widget_radius = theme.border_radius
 
--- Tasklist widget
+-- Titlebars
+theme.titlebar_enabled = false
+
+-- Tooltip
+theme.tooltip_bg = theme.black
+theme.tooltip_fg = theme.xforeground
+theme.tooltip_border_color = theme.focus
+theme.tooltip_border_width = dpi(1)
+theme.tooltip_font = theme.font_name .. '12'
+
+-- Tasklist
 theme.tasklist_bg_normal = theme.transparent
 theme.tasklist_bg_focus = theme.accent
 theme.tasklist_bg_minimize = theme.magenta
 
 -- Taglist
-theme.taglist_bg_normal = theme.mars
+theme.taglist_spacing = dpi(2)
+theme.taglist_shape = helpers.rrect(theme.widget_radius)
+
+theme.taglist_squares_sel = nil
+theme.taglist_squares_unsel = nil
 
 theme.taglist_bg_focus = theme.focus
 theme.taglist_fg_focus = theme.xforeground
@@ -124,62 +130,6 @@ theme.taglist_bg_empty = theme.black
 theme.taglist_fg_empty = theme.xbackground
 
 theme.taglist_bg_volatile = theme.jupiter
-theme.taglist_fg_volatile = theme.magenta
-theme.taglist_spacing = dpi(2)
-
-theme.taglist_squares_sel = nil
-theme.taglist_squares_unsel = nil
-
-theme.taglist_shape_focus = helpers.rrect(theme.widget_radius)
-theme.taglist_shape_empty = helpers.rrect(theme.widget_radius)
-theme.taglist_shape = helpers.rrect(theme.widget_radius)
-theme.taglist_shape_urgent = helpers.rrect(theme.widget_radius)
-theme.taglist_shape_volatile = helpers.rrect(theme.widget_radius)
-
--- Titlebars
-theme.titlebar_enabled = false
-theme.titlebar_size = dpi(40)
-theme.titlebar_color = theme.focus
-theme.titlebar_unfocused = theme.yellow
-
--- Pop up notifications
-theme.popup_size = dpi(188)
-theme.pop_bg = theme.xbackground
-theme.pop_volume_color = theme.accent
-theme.pop_brightness_color = theme.venus
-theme.popup_border_radius = theme.border_radius
-
--- Tooltip
-theme.tooltip_bg = theme.black
-theme.tooltip_fg = theme.xforeground
-theme.tooltip_border_color = theme.focus
-theme.tooltip_border_width = dpi(1)
-theme.tooltip_font = theme.font_name .. '12'
-
--- Layout icons
-theme.layout_tile = local_dir .. "icons/layouts/tile.svg"
-theme.layout_tilebottom = local_dir .. "icons/layouts/tilebottom.svg"
-theme.layout_magnifier = local_dir .. "icons/layouts/magnifier.svg"
-theme.layout_max = local_dir .. "icons/layouts/max.svg"
-theme.layout_horizontal = local_dir .. "icons/layouts/horizontal.svg"
-theme.layout_spiral = local_dir .. "icons/layouts/spiral.svg"
-theme.layout_mstab = local_dir .. "icons/layouts/mstab.svg"
-theme.layout_deck = local_dir .. "icons/layouts/deck.svg"
-theme.layout_floating = local_dir .. "icons/layouts/floating.svg"
-
--- Edge snap
-theme.snap_bg = theme.cyan
-theme.snap_shape = helpers.rrect(theme.client_border_radius)
-
--- Control center
-theme.control_center_width = dpi(360)
-theme.control_center_button_bg = theme.focus
-
--- Playerctl
-theme.playerctl_ignore = {"qutebrowser", "chromium", "brave"}
-theme.playerctl_player = {"youtube-music", "spotify", "mpd", "%any"}
-theme.playerctl_update_on_activity = true
-theme.playerctl_position_update_interval = 1
 
 -- Mainmenu
 theme.menu_font = theme.font_name .. "medium 10"
@@ -203,17 +153,9 @@ theme.hotkeys_font = theme.mono_font
 theme.hotkeys_description_font = theme.font_name .. "10"
 theme.hotkeys_group_margin = dpi(12)
 
--- Layout List
-theme.layoutlist_border_color = theme.focus
-theme.layoutlist_border_width = theme.border_width
-theme.layoutlist_bg_selected = theme.xbackground
-theme.layoutlist_shape_border_width_selected = theme.border_width
-theme.layoutlist_shape_border_color_selected = theme.focus
-theme.layoutlist_shape_selected = helpers.rrect(theme.border_radius / 2)
-
--- Gaps
-theme.useless_gap = dpi(4)
-
+-----------------
+-- UI Elements --
+-----------------
 -- Wibar
 theme.wibar_size = dpi(40)
 theme.wibar_bg = theme.xbackground
@@ -229,6 +171,79 @@ theme.wibar_margins = {
 }
 theme.wibar_shape = helpers.rrect(theme.client_border_radius)
 
+-- Pop up notifications
+theme.popup_size = dpi(188)
+theme.pop_bg = theme.xbackground
+theme.pop_volume_color = theme.accent
+theme.pop_brightness_color = theme.venus
+theme.popup_border_radius = theme.border_radius
+
+-- Layout list popup
+theme.layoutlist_border_color = theme.focus
+theme.layoutlist_border_width = theme.border_width
+theme.layoutlist_bg_selected = theme.xbackground
+theme.layoutlist_shape_border_width_selected = theme.border_width
+theme.layoutlist_shape_border_color_selected = theme.focus
+theme.layoutlist_shape_selected = helpers.rrect(theme.border_radius / 2)
+
+-- Layout icons
+theme.layout_tile = local_dir .. "icons/layouts/tile.svg"
+theme.layout_tilebottom = local_dir .. "icons/layouts/tilebottom.svg"
+theme.layout_magnifier = local_dir .. "icons/layouts/magnifier.svg"
+theme.layout_max = local_dir .. "icons/layouts/max.svg"
+theme.layout_horizontal = local_dir .. "icons/layouts/horizontal.svg"
+theme.layout_spiral = local_dir .. "icons/layouts/spiral.svg"
+theme.layout_mstab = local_dir .. "icons/layouts/mstab.svg"
+theme.layout_deck = local_dir .. "icons/layouts/deck.svg"
+theme.layout_floating = local_dir .. "icons/layouts/floating.svg"
+
+-- Edge snap
+theme.snap_bg = theme.cyan
+theme.snap_shape = helpers.rrect(theme.client_border_radius)
+
+-- Control center
+theme.control_center_width = dpi(360)
+theme.control_center_button_bg = theme.focus
+-- PFP
+theme.pfp = gsurface.load_uncached(local_dir .. "theme/assets/kirby_snowboard.jpg")
+
+-- Notifications
+naughty.config.defaults.ontop = true
+naughty.config.defaults.title = "System Notification title"
+
+naughty.config.icon_dirs = {
+    HOME .. "/.local/share/icons/Kuyen-icons/apps/48/", HOME .. "/.local/share/icons/Kuyen-icons/",
+    "/usr/share/icons/Papirus-Dark/", "/usr/share/icons/Suru++-Asprómauros/",
+    "/usr/share/icons/breeze-dark/", "/usr/share/pixmaps/"
+}
+naughty.config.icon_formats = {"png", "svg", "jpg", "gif"}
+
+theme.notification_spacing = 12
+theme.notification_border_radius = theme.border_radius / 2
+theme.notification_font = theme.font_name .. 12
+theme.notification_position = "top_right"
+
+-- Notif center
+theme.notif_center_radius = theme.border_radius
+
+-- Notification icons
+theme.notification_icon = gsurface.load_uncached(local_dir .. "icons/notification.svg")
+theme.notification_bell_icon = gsurface.load_uncached(local_dir .. "icons/notification_bell.svg")
+
+-- Systray
+theme.systray_icon_spacing = dpi(4)
+theme.systray_max_rows = 1
+theme.bg_systray = theme.wibar_bg
+
+-------------
+--- Bling ---
+-------------
+-- Playerctl
+theme.playerctl_ignore = {"qutebrowser", "chromium", "brave"}
+theme.playerctl_player = {"youtube-music", "spotify", "mpd", "%any"}
+theme.playerctl_update_on_activity = true
+theme.playerctl_position_update_interval = 1
+
 -- Tabs
 theme.mstab_bar_height = dpi(42)
 theme.mstab_bar_padding = dpi(2)
@@ -243,31 +258,6 @@ theme.tabbar_AA_radius = dpi(2)
 theme.tabbar_size = 40
 theme.mstab_bar_ontop = true
 
--- Notifications
-naughty.config.defaults.ontop = true
-naughty.config.defaults.title = "System Notification title"
-
-naughty.config.icon_dirs = {
-    HOME .. "/.local/share/icons/Kuyen-icons/apps/48/", HOME .. "/.local/share/icons/Kuyen-icons/",
-    "/usr/share/icons/Papirus-Dark/", "/usr/share/icons/Suru++-Asprómauros/",
-    "/usr/share/icons/breeze-dark/", "/usr/share/pixmaps/"
-}
-naughty.config.icon_formats = {"png", "svg", "jpg", "gif"}
-
-theme.notification_spacing = 16
-theme.notification_border_radius = theme.border_radius / 2
-theme.notification_font = theme.font_name .. " 12"
-theme.notification_position = "top_right"
-
--- Notif center
-theme.notif_center_radius = theme.border_radius
-
--- Systray
-theme.systray_icon_spacing = dpi(4)
-theme.systray_max_rows = 1
-theme.bg_systray = theme.wibar_bg
-
---- Bling ---
 -- Tag Preview
 theme.tag_preview_client_border_radius = theme.client_border_radius
 theme.tag_preview_client_opacity = 1
@@ -297,8 +287,8 @@ theme.window_switcher_name_font = theme.font_name .. "11"
 theme.window_switcher_name_focus_color = theme.cyan
 theme.window_switcher_icon_width = dpi(28)
 
--- video wallpaper
-theme.video_wallpaper_path = HOME .. "/Vídeos/aniwalls/unicorn_loop.mp4"
-theme.video_wallpaper_vertical_path = HOME .. "/Vídeos/aniwalls/6am_shinjuku_guttykreum.mp4"
+-- Video wallpaper
+theme.video_wallpaper_path = HOME .. "/Vídeos/aniwalls/violet-afternoon-w.mp4"
+theme.video_wallpaper_vertical_path = HOME .. "/Vídeos/aniwalls/violet-afternoon-v.mp4"
 
 return theme
