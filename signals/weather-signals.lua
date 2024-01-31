2840f6d961716f3e4047e814888384a3f792f4e8
pablonoya <pablonoya18@live.com>
2024-01-31 19:56:53 -0400

    check internet connection before requesting weather

--- signals/weather-signals.lua
+++ signals/weather-signals.lua
@@ -1,6 +1,7 @@
 local spawn = require("awful.spawn")
 local beautiful = require("beautiful")
 local gtimer = require("gears.timer")
+
 local json = require("away.third_party.dkjson")
 
 local endpoint = "https://api.openweathermap.org/data/2.5/weather"
@@ -21,23 +22,29 @@ local function table_to_query_string(query_params)
     end
 
     return table.concat(query_string, "&")
-
 end
 
-local function emit_weather_signal()
-    local cmd = string.format("curl -s -m 7 '%s?%s'", endpoint, table_to_query_string(query_params))
-    spawn.easy_async_with_shell(
-        cmd, function(stdout, stderr)
-            local data = json.decode(stdout)
+local function emit_weather_signal(stdout, stderr)
+    awesome.emit_signal("weather::update", json.decode(stdout))
+end
 
-            awesome.emit_signal("weather::update", data)
-        end
-    )
+local function check_internet_connection(stdout, stderr)
+    if stderr == "" then
+        -- Internet connection is available
+        local command = string.format(
+            "curl -s -m 7 '%s?%s'", endpoint, table_to_query_string(query_params)
+        )
+        spawn.easy_async_with_shell(command, emit_weather_signal)
+    else
+        gtimer.delayed_call(check_internet_connection, 5)
+    end
 end
 
 local timer = gtimer {
     timeout = 60 * 30,
     call_now = true,
     autostart = true,
-    callback = emit_weather_signal
+    callback = function()
+        spawn.easy_async_with_shell("ping -c 1 8.8.8.8", check_internet_connection)
+    end
 }
