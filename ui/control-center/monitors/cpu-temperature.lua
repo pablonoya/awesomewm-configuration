e209aa6f4a05c443079db0fbe46e70bb13af62fa
pablonoya <pablonoya18@live.com>
2024-01-31 12:54:20 -0400

    remove asusctl fan profile change

--- ui/control-center/monitors/cpu-temperature.lua
+++ ui/control-center/monitors/cpu-temperature.lua
@@ -1,23 +1,8 @@
 local spawn = require("awful.spawn")
 local beautiful = require("beautiful")
-local wibox_container = require("wibox.container")
-local wibox_widget = require("wibox.widget")
 
 local monitor_progressbar = require("ui.widgets.monitor-progressbar")
 local text_icon = require("ui.widgets.text-icon")
-
-local function change_asus_fan_profile(temp)
-    if temp < 85 then
-        return
-    end
-    spawn.easy_async(
-        "asusctl profile -p", function(stdout)
-            if stdout:match("Quiet") then
-                spawn("asusctl profile -P Balanced")
-            end
-        end
-    )
-end
 
 local meter_icon = text_icon {
     text = "\u{e1ff}"
@@ -25,8 +10,6 @@ local meter_icon = text_icon {
 
 local function format_info(stdout)
     local temp = stdout:match("%d+") / 1000
-    change_asus_fan_profile(temp)
-
     return temp, string.format("%.1f °", temp)
 end
 
@@ -34,7 +17,7 @@ local cpu_temperature = monitor_progressbar {
     name = "CPU",
     icon_widget = meter_icon,
     info = "1 °",
-    min_value = Z0,
+    min_value = 10,
     max_value = 100,
     slider_color = {
         type = "linear",
