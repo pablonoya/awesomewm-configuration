4e180e5ada57b2d3f94da0194a55034dc7aaf7f9
pablonoya <pablonoya18@live.com>
2024-03-03 00:40:14 -0400

    rename night light signal

--- signals/redshift.lua
+++ signals/redshift.lua
@@ -2,6 +2,6 @@ local spawn = require("awful.spawn")
 
 spawn.easy_async_with_shell(
     "pgrep redshift", function(stdout)
-        awesome.emit_signal("blue_light:state", stdout ~= "")
+        awesome.emit_signal("nightlight::state", stdout ~= "")
     end
 )
--- ui/control-center/controls/toggle_buttons/night_light.lua
+++ ui/control-center/controls/toggle_buttons/night_light.lua
@@ -3,21 +3,19 @@ local beautiful = require("beautiful")
 
 local toggle_button = require("ui.widgets.toggle_button")
 
-local signal_label = "blue_light:state"
+local signal_label = "nightlight::state"
 
 local function onclick()
     spawn.easy_async_with_shell(
         string.format(
             [[
             if [ ! -z $(pgrep redshift) ]; then
-                pkill redshift &
-                echo 'OFF'
+                pkill redshift & echo 'OFF'
             else
-                redshift -l %s:%s -t 6500:4200 &>/dev/null &
-                echo 'ON'
+                redshift -l %s:%s -t 6500:4200 &>/dev/null & echo 'ON'
             fi ]], beautiful.latitude, beautiful.longitude
         ), function(stdout)
-            awesome.emit_signal(signal_label, stdout:match("ON"))
+            awesome.emit_signal(signal_label, stdout:match("ON") == "ON")
         end
     )
 end
