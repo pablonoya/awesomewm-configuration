9d8b831f711387b8134033c6a7eff330d650b7e8
pablonoya <pablonoya18@live.com>
2024-04-02 22:40:16 -0400

    refactor device_indicators

--- ui/bar/widgets/device_indicators.lua
+++ ui/bar/widgets/device_indicators.lua
@@ -1,42 +1,40 @@
-local awful = require("awful")
+local awful_tooltip = require("awful.tooltip")
 local beautiful = require("beautiful")
 local gears = require("gears")
 local wibox = require("wibox")
 
 local system_controls = require("helpers.system_controls")
-local text_icon = require("ui.widgets.text-icon")
 local helpers = require("helpers")
 
-local mic_icon = wibox.widget {
-    text_icon {
-        text = "\u{e029}",
-        size = 16
-    },
-    visible = false,
-    margins = dpi(2),
-    widget = wibox.container.margin
+local text_icon = require("ui.widgets.text-icon")
+
+local mic_icon = text_icon {
+    text = "\u{e029}",
+    size = 16
 }
 
-local device_name = awful.tooltip {
-    objects = {mic_icon},
-    text = "Microphone"
-}
-
-awful.spawn.easy_async(
-    "pamixer --default-source --get-mute", function(stdout)
-        awesome.emit_signal("microphone::mute", stdout:match("true"))
-    end
-)
+helpers.add_action(mic_icon, system_controls.mic_toggle)
 
 awesome.connect_signal(
     "microphone::muted", function(muted)
         mic_icon.text = muted and "\u{e02b}" or "\u{e029}"
     end
 )
+local mic_indicator = wibox.widget {
+    mic_icon,
+    visible = false,
+    margins = dpi(2),
+    widget = wibox.container.margin
+}
+
+local mic_device_name = awful_tooltip {
+    objects = {mic_indicator},
+    text = "Microphone"
+}
 
 awesome.connect_signal(
     "microphone::device", function(name)
-        device_name.text = name:gsub("\n", "")
+        mic_device_name.text = name:gsub("\n", "")
     end
 )
 
@@ -54,29 +52,32 @@ local device_indicators = wibox.widget {
     {
         {
             {
-                cam_icon,
-                mic_icon,
-                layout = wibox.layout.fixed.horizontal
+                {
+                    cam_icon,
+                    mic_indicator,
+                    layout = wibox.layout.fixed.horizontal
+                },
+                left = dpi(2),
+                right = dpi(2),
+                widget = wibox.container.margin
             },
-            left = dpi(2),
-            right = dpi(2),
-            widget = wibox.container.margin
+            fg = beautiful.black,
+            bg = beautiful.green,
+            forced_height = dpi(28),
+            shape = gears.shape.rounded_bar,
+            widget = wibox.container.background
         },
-        fg = beautiful.black,
-        bg = beautiful.green,
-        forced_height = dpi(28),
-        shape = gears.shape.rounded_bar,
-        widget = wibox.container.background
+        widget = wibox.container.place
     },
     visible = false,
-    widget = wibox.container.place
+    left = dpi(4),
+    right = dpi(4),
+    widget = wibox.container.margin
 }
-
-helpers.add_action(mic_icon, system_controls.mic_toggle)
 
 awesome.connect_signal(
     "microphone::state", function(state)
-        mic_icon.visible = state
+        mic_indicator.visible = state
         device_indicators.visible = state or cam_icon.visible
     end
 )
@@ -88,9 +89,4 @@ awesome.connect_signal(
     end
 )
 
-return {
-    device_indicators,
-    left = dpi(4),
-    right = dpi(4),
-    widget = wibox.container.margin
-}
+return device_indicators
