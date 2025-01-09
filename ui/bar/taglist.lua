14f7115f241f2e8b418e17a51480065773a4ce98
pablonoya <pablonoya18@live.com>
2025-01-09 12:34:09 -0400

    Add shrink animation to tasklist when clienticon is clicked

--- ui/bar/taglist.lua
+++ ui/bar/taglist.lua
@@ -1,11 +1,26 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 local wibox = require("wibox")
-
 local helpers = require("helpers")
+
+local modkeys = require("bindings.modkeys")
+local rubato = require("module.rubato")
+
 local border_container = require("ui.widgets.border-container")
 
-local modkey = "Mod4"
+local modkey, alt, ctrl, shift = table.unpack(modkeys)
+
+local function change_width(widget, init, target)
+    local timed = rubato.timed {
+        duration = 0.1,
+        pos = init,
+        subscribed = function(pos)
+            widget.forced_height = pos
+            widget.forced_width = pos
+        end
+    }
+    timed.target = target
+end
 
 local client_filter = function(t)
     return function(c)
@@ -21,11 +36,11 @@ end
 local tasklist = function(t)
     local s = awful.screen.focused()
     return awful.widget.tasklist {
-        screen = s,
+        screen = t.screen,
         filter = client_filter(t),
         buttons = {
             awful.button(
-                {}, 1, function(c)
+                {}, awful.button.names.LEFT, function(c)
                     if c == client.focus then
                         c.minimized = true
                     else
@@ -35,7 +50,7 @@ local tasklist = function(t)
                     end
                 end
             ), awful.button(
-                {}, 2, function(c)
+                {}, awful.button.names.MIDDLE, function(c)
                     c:kill()
                 end
             )
@@ -53,6 +68,17 @@ local tasklist = function(t)
                     forced_width = dpi(22),
                     widget = awful.widget.clienticon
                 },
+                buttons = {
+                    awful.button(
+                        {}, awful.button.names.LEFT, function(self)
+                            change_width(self.widget.children[1], dpi(22), dpi(16))
+                        end, function(self)
+                            change_width(self.widget.children[1], dpi(16), dpi(22))
+                        end
+                    )
+                },
+                fill_vertical = true,
+                forced_width = dpi(22),
                 widget = wibox.container.place
             },
             {
@@ -61,7 +87,7 @@ local tasklist = function(t)
                 widget = wibox.container.background
             },
             layout = wibox.layout.align.vertical,
-            create_callback = function(self, c, index, objects)
+            create_callback = function(self, c)
                 -- BLING: Toggle the popup on hover and disable it off hover
                 self:connect_signal(
                     'mouse::enter', function()
@@ -81,33 +107,27 @@ end
 
 local taglist_buttons = {
     awful.button(
-        {}, 1, function(t)
+        {}, awful.button.names.LEFT, function(t)
             t:view_only()
         end
     ), awful.button(
-        {modkey}, 1, function(t)
+        {shift}, awful.button.names.LEFT, function(t)
             if client.focus then
                 client.focus:move_to_tag(t)
             end
         end
     ), awful.button(
-        {}, 3, function(t)
+        {ctrl}, awful.button.names.LEFT, function(t)
             if client.focus then
                 client.focus:toggle_tag(t)
             end
         end
     ), awful.button(
-        {modkey}, 3, function(t)
-            if client.focus then
-                client.focus:toggle_tag(t)
-            end
-        end
-    ), awful.button(
-        {}, 4, function(t)
+        {}, awful.button.names.SCROLL_UP, function(t)
             awful.tag.viewnext(t.screen)
         end
     ), awful.button(
-        {}, 5, function(t)
+        {}, awful.button.names.SCROLL_DOWN, function(t)
             awful.tag.viewprev(t.screen)
         end
     )
@@ -152,36 +172,35 @@ local function create_taglist_callback(self, t, index, _)
         "mouse::enter", function()
             if #t:clients() > 0 then
                 awesome.emit_signal("bling::tag_preview::update", t)
-                awesome.emit_signal("bling::tag_preview::visibility", s, true)
+                awesome.emit_signal("bling::tag_preview::visibility", t.screen, true)
             end
         end
     )
     self:connect_signal(
         "mouse::leave", function()
-            awesome.emit_signal("bling::tag_preview::visibility", s, false)
+            awesome.emit_signal("bling::tag_preview::visibility", t.screen, false)
         end
     )
     helpers.add_hover_cursor(self)
     adjust_grid_layout(t.screen)
 end
 
-local taglist = function(s)
+return function(s)
     local is_horizontal = s.geometry.height < s.geometry.width
 
     local custom_taglist = awful.widget.taglist {
         screen = s,
         filter = awful.widget.taglist.filter.noempty,
         layout = is_horizontal and wibox.layout.fixed.horizontal or grid_layout,
+        buttons = taglist_buttons,
         widget_template = {
             {
                 {
                     -- tag
                     {
-
                         id = "index_role",
                         font = beautiful.font_name .. " Bold 14",
                         widget = wibox.widget.textbox
-
                     },
                     -- tasklist
                     {
@@ -198,10 +217,8 @@ local taglist = function(s)
             },
             id = "background_role",
             widget = wibox.container.background,
-
             create_callback = create_taglist_callback
-        },
-        buttons = taglist_buttons
+        }
     }
 
     -- Update column and row number if vertical screen
@@ -217,5 +234,3 @@ local taglist = function(s)
         widget = custom_taglist
     }
 end
-
-return taglist
